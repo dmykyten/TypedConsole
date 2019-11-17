b0bc54717e1dbd13b969a0e0228ecfefab35964f
Dmytro Mykytenko <50519414+dmykyten@users.noreply.github.com>
2019-11-17 17:38:30 +0200

    Initial commit

---
+++ Dmykyt.Console.Input/TypedConsole.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Dmykyt.Console.Input
+{
+    public class TypedConsole
+    {
+        static public T read<T>(Func<String, T> Convert, string message = null)
+        {
+            if (message == null) {
+
+                System.Console.Write("Please enter " + typeof(T).Name + ":");
+
+            }
+            else
+            {
+                System.Console.Write(message + ":");
+            }
+            var userInput = System.Console.ReadLine();
+            try
+            {
+                return Convert(userInput);
+            }
+            catch
+            {
+                System.Console.WriteLine("Error.");
+                return read<T>(Convert, message);
+            }
+        }
+
+        static public T read<T>(string message = null)
+        {
+            switch (typeof(T).Name)
+            {
+                case "Boolean":
+                    return (T)(object)read(Boolean.Parse, message);
+                case "Byte":
+                    return (T)(object)read(Byte.Parse, message);
+                case "Char":
+                    return (T)(object)read(Char.Parse, message);
+                case "DateTime":
+                    return (T)(object)read(DateTime.Parse, message);
+                case "Decimal":
+                    return (T)(object)read(Decimal.Parse, message);
+                case "Double":
+                    return (T)(object)read(Double.Parse, message);
+                case "Int16":
+                    return (T)(object)read(Int16.Parse, message);
+                case "Int32":
+                    return (T)(object)read(Int32.Parse, message);
+                case "Int64":
+                    return (T)(object)read(Int64.Parse, message);
+                case "SByte":
+                    return (T)(object)read(SByte.Parse, message);
+                case "UInt16":
+                    return (T)(object)read(UInt16.Parse, message);
+                case "UInt32":
+                    return (T)(object)read(UInt32.Parse, message);
+                case "UInt64":
+                    return (T)(object)read(UInt64.Parse, message);
+                default:
+                    throw new NotSupportedException("This " + typeof(T).Name + " is not supported");
+            }
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dmykyt.Console.Input;
+
+namespace Test
+{
+    class Program
+    {
+        public delegate int RoundDelegate(decimal arg);
+        public static Func<String, decimal> ConvertDoSomething;
+
+        static void Main(string[] args)
+        {
+            TypedConsole.read<int>(int.Parse);
+            TypedConsole.read<int>(int.Parse,"amount of boxes");
+            TypedConsole.read<int>();
+        }
+
+        static void DelegateExamples(string s)
+        {
+            ConvertDoSomething = (String som) => Decimal.Parse(som + ".01");
+
+            ConvertDoSomething = Decimal.Parse;
+            ConvertDoSomething = func;
+
+            ConvertDoSomething("Hello");
+
+            RoundDelegate rounder;
+            rounder = RoundUp;
+            rounder = Round;
+            rounder = RoundUp;
+            rounder = Round;
+
+            Func<decimal, int> rounder2;
+            rounder2 = RoundUp;
+            rounder2((decimal)1.7);
+            var a = rounder((decimal)1.2);
+
+            Action<String> Print;
+            Print = Console.WriteLine;
+            Print("ss");
+
+            int aaa;
+
+            // TypedConsole<decimal>.read<int>();
+        }
+
+        static decimal func(string s)
+        {
+            Console.Write(s);
+            return 15;
+        }
+        static int Round(decimal number)
+        {
+            return (int)Math.Round(number);
+        }
+        static int RoundUp(decimal number)
+        {
+            return (int)Math.Round(number + (decimal)0.5);
+        }
+    }
+}
